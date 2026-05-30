2d5c60967be4d29bce81ccb2412a3132fbe028d4
Youssef Hany <youssifhany75@gmail.com>
2026-05-30 20:06:41 +0300

    feat: make a dummy html page

--- server/index.js
+++ server/index.js
@@ -5,7 +5,25 @@ const app = express();
 app.use(express.static(path.join(__dirname, "../client/build")));
 
 app.get("/", (_, res) => {
-  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.write(`
+    
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Document</title>
+    </head>
+    <body>
+        <div>
+        <h1>Hi, I'm Youssif</h1>
+        <p>I'm a web developer and UI designer</p>
+        </div>
+    </body>
+    </html>
+    `);
+  res.end();
 });
 
 const PORT = 3000;
